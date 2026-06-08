90e3cc003ec4f8a5f66a1fe6b4c0fa0bb65eb7b0
karenherrerar <karen.herrerar@udem.edu>
2026-06-08 14:10:30 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -22,17 +22,85 @@
 unsigned char Estado1,Estado2,Estado3;
 
 void main(void){
-    ANSEL=0;
+    ANSEL=0; 
     ANSELH=0;
     
     OPTION_REG=OPTION_REG & 0b01111111;
     
     TRISB=0xFF;
-    TRISD=0;
+    TRISD=0x00;
+    TRISC=0x00;
+    PORTC=0;
+    PORTD=0;
+    PORTB=0;
     
+    unsigned char cont=0;
+    unsigned char unidades, decenas;
+
+    const unsigned char display[10] = {
+        0x3F, //0
+        0x06, //1
+        0x5B, //2
+        0x4F, //3
+        0x66, //4
+        0x6D, //5
+        0x7D, //6
+        0x07, //7
+        0x7F, //8
+        0x6F  //9
+    };
     while(1){
-        PORTDbits.RD0 = !PORTBbits.RB0;
-        PORTDbits.RD1= !PORTBbits.RB1;
-        PORTDbits.RD2= !PORTBbits.RB2;
-    }
+        
+    // Botón sumar
+    if(PORTBbits.RB0 == 0){
+
+        __delay_ms(20);
+
+        if(PORTBbits.RB0 == 0){
+
+            cont++;
+
+            if(cont > 99)
+                cont = 0;
+
+            while(PORTBbits.RB0 == 0);
+        };
+    };
+
+    // Botón restar
+    if(PORTBbits.RB1 == 0){
+
+        __delay_ms(20);
+
+        if(PORTBbits.RB1 == 0){
+
+            if(cont == 0)
+                cont = 99;
+            else
+                cont--;
+
+            while(PORTBbits.RB1 == 0);
+        };
+    };
+
+    // Botón reset
+    if(PORTBbits.RB2 == 0){
+
+        __delay_ms(20);
+
+        if(PORTBbits.RB2 == 0){
+
+            cont = 0;
+
+            while(PORTBbits.RB2 == 0);
+        };
+    };
+    
+    decenas = cont / 10;
+    unidades = cont % 10;
+    PORTD = display[decenas];
+    PORTC = display[unidades];
+        
+    };
 };
+
